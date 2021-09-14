7a17517a4c62d9a8e35d0c96a8e1d96029dd49da
marshzhao <75262496+marshzhao@users.noreply.github.com>
2021-09-14 18:17:52 +0800

    Update baidu_hot.py

--- baidu_hot.py
+++ baidu_hot.py
@@ -61,8 +61,8 @@ def send_email(body):
 #获取内容函数
 def get_page(url):
     try:
-        user_agent={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"}
-        r=requests.get(url=url,headers=user_agent)
+        #此处不使用user agent也可以正常访问
+        r=requests.get(url=url)
         r.raise_for_status()
         r.encoding=r.apparent_encoding
         return r.text
